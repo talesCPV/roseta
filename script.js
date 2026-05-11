836890a97f92dad3fa91d9c1afd834dce67b11a0
talesCPV <tales@flexibus.com.br>
2026-05-11 12:00:25 -0300

    by_script

--- script.js
+++ script.js
@@ -6,16 +6,25 @@ document.querySelector('.modal-close').addEventListener('click',()=>{
     showModal()
 })
 
-function showModal(title=''){
+function showModal(title='',body=null, script=null){
     document.querySelector('.modal').style.display = title.length ? 'flex' : 'none'
-    document.querySelector('.modal-title').innerHTML = title
+    try{
+        document.querySelector('.modal-title').innerHTML = title
+        document.querySelector('.modal-content').innerHTML = body.innerHTML
+        eval(script.innerHTML);    
+    }catch{null}
 }
 
-function openHTML(template,modal='pop-up'){
-    const frm = fetch(`templates/${template}.html`)
+function openHTML(template){
+    fetch(`templates/${template}.html`)
     .then( stream => stream.text())
     .then( text => {
-        console.log(text)
+        const temp = document.createElement('div');
+        temp.innerHTML = text;
+        const title = temp.getElementsByTagName('title')[0];
+        const body = temp.getElementsByTagName('template')[0];
+        const script = temp.getElementsByTagName('script')[0] != undefined ? temp.getElementsByTagName('script')[0] : ''
+        showModal(title.innerText,body,script)
     })
 
 }
