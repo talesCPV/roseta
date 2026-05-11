21b7d740f648747b8d034827f79c55371611e3ca
talesCPV <tales@flexibus.com.br>
2026-05-11 09:36:07 -0300

    by_script

---
+++ commit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Upload files to Github - git@github.com:talesCPV/roseta.git
+
+read -p "Are you sure to commit Roseta Project to GitHub ? (Y/n)" -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+    git init
+
+#    git add assets/
+    git add script.js
+    git add style.css
+    git add index.html
+#    git add teste.html
+    git add commit.sh
+    git add data.json
+    
+    git commit -m "by_script"
+
+    git branch -M main
+    git remote add origin git@github.com:talesCPV/roseta.git
+    git remote set-url origin git@github.com:talesCPV/roseta.git
+
+    git push -u -f origin main
+
+fi
