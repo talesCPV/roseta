d75740d9845ec243a8dcf7f2f046c6eca578965b
talesCPV <tales@flexibus.com.br>
2026-05-31 18:41:06 -0300

    by_script

--- scripts/window.js
+++ scripts/window.js
@@ -1,4 +1,4 @@
-function openHTML(template,data={},dimension=[0,0]){
+function openHTML(template,data={},dimension=[-1,-1]){
     fetch(`templates/${template}.html`)
     .then( stream => stream.text())
     .then( text => {
@@ -7,7 +7,7 @@ function openHTML(template,data={},dimension=[0,0]){
         const title = temp.getElementsByTagName('title')[0];
         const body = temp.getElementsByTagName('template')[0];
         const script = temp.getElementsByTagName('script')[0] != undefined ? temp.getElementsByTagName('script')[0] : ''
-        newCategoria(template,title,body,script,data,dimension)
+        newWindow(template,title,body,script,data,dimension)
     })
 }
 
@@ -16,36 +16,35 @@ function closeWindow(id){
     delete(main_data[id])
 }
 
-function newCategoria(id,ttl,body,script,data={},dimension){
-
+function newWindow(id,ttl,body,script,data={},dimension){
     try{
-        const area = document.querySelector('.categoria-area')
+        const area = document.querySelector('.window-area')
     
         main_data[id] = data
     
-        const categoria =  document.createElement('div')
-        categoria.className = 'categoria'
-        categoria.id = id
+        const wd =  document.createElement('div')
+        wd.className = 'window'
+        wd.id = id
     
         const form = document.createElement('div')
-        form.className = 'categoria-form'
-        if(dimension[0] >0 && dimension[1]>0){
-            form.style.width = `${dimension[0]}px`
-            form.style.height = `${dimension[1]}px`
+        form.className = 'window-form'
+        if(dimension[0] >=0 && dimension[1]>=0){
+            form.style.width =  dimension[0] ? `${dimension[0]}px` : 'auto'
+            form.style.height = dimension[1] ? `${dimension[1]}px` : 'auto'
         }
-        categoria.appendChild(form)
+        wd.appendChild(form)
     
         const head = document.createElement('div')
-        head.className = 'categoria-head'
+        head.className = 'window-head'
         form.appendChild(head)
     
         const title = document.createElement('span')
-        title.className = 'categoria-title'
+        title.className = 'window-title'
         title.innerHTML = ttl.innerText
         head.appendChild(title)
     
         const close = document.createElement('span')
-        close.className = 'categoria-close'
+        close.className = 'window-close'
         close.innerHTML = '&times;'
         close.addEventListener('click',()=>{
             closeWindow(id)
@@ -53,11 +52,10 @@ function newCategoria(id,ttl,body,script,data={},dimension){
         head.appendChild(close)
     
         const content = document.createElement('div')
-        content.className = 'categoria-content'
+        content.className = 'window-content'
         content.innerHTML = body.innerHTML
         form.appendChild(content)
-    
-        area.appendChild(categoria)
+        area.appendChild(wd)
         eval(script.innerHTML)
     }catch{null}
 
